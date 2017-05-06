c45347eed0fbcbe1bcbcd782fb58a4e9bb645397
avijeet <a@avijeets.com>
2017-05-06 02:22:00 -0400

    pragma for loop

--- spell_t4_singleloop.c
+++ spell_t4_singleloop.c
@@ -56,6 +56,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
+	#pragma omp parallel for schedule(guided, 256) shared(bv_size, num_hf, wl, wl_size) private(i, j, hash)
 	for (i = 0; i < wl_size; i++) {
 		for (j = 0; j < num_hf; j++) {
 			hash = hf[j] (get_word(wl, i));
